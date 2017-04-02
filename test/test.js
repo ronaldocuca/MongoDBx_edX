6bee9faf2ea66ab57c742ce5f5bf595cccbff341
root <root@scotchbox>
2017-04-02 20:01:56 +0000

    test

---
+++ test/test.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+
+// describe('my feature', function(){
+//   if('works', function() {
+//     assert.equal('A', 'A');
+//   });
+
+//   if('fails gracefully', function(){
+//     assert.throws(function(){
+//       throw 'Error!';
+//     });
+//   });
+// });
+
+// describe('my other feature', function(){
+//   if('async', function(done) {
+//     setTimeout(function() {
+//       done();
+//     }, 25);
+//   });
+// });
+ describe('my feature', function() {
+  it('works', function() {});
+  it('fails gracefully', function() {});
+});
+describe('my other feature', function() {
+  it('sorta works', function() {});
+  });
